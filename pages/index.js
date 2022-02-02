4ec38ec027bb342685e74b73fa71abbb936f52a6
Ariane <silvaariane@hotmail.com>
2022-02-02 20:32:33 -0300

    adiçao de funcionalidade de infos do evento e alteção de cores do tema

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,53 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from '../config.json';
 
 function GlobalStyle() {
   return (
     <style global jsx>{`
-    * {
-      margin: 0;
-      padding: 0;
-      box-sizing: border-box;
-      list-style: none;
-    }
-
-    body {
-      font-family: 'Open Sans', sans-serif;
-    }
-    /* App fit Height */
-    html, body, #__next {
-      min-height: 100vh;
-      display: flex;
-      flex: 1;
-    }
-    #__next {
-      flex: 1;
-    }
-    #__next > * {
-      flex: 1;
-    }
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        list-style: none;
+      }
+      body {
+        font-family: 'Open Sans', sans-serif;
+      }
+      /* App fit Height */ 
+      html, body, #__next {
+        min-height: 100vh;
+        display: flex;
+        flex: 1;
+      }
+      #__next {
+        flex: 1;
+      }
+      #__next > * {
+        flex: 1;
+      }
+      /* ./App fit Height */ 
     `}</style>
-  )
+  );
 }
 
-function Title(props) {
-  console.log(props);
-  const Tag = props.tag;
+function Titulo(props) {
+  const Tag = props.tag || 'h1';
   return (
     <>
       <Tag>{props.children}</Tag>
       <style jsx>{`
-    ${Tag} {
-      color: ${appConfig.theme.colors.neutrals['200']};
-      font-size: 24px;
-      font-weight: 600;
-    }
-    `}</style>
+            ${Tag} {
+                color: ${appConfig.theme.colors.primary[100]};
+                font-size: 22.5px;
+                font-weight: 600;
+            }
+            `}</style>
     </>
   );
 }
+
 
 // function HomePage() {
 //  return (
@@ -60,16 +62,16 @@ function Title(props) {
 // export default HomePage
 
 export default function PaginaInicial() {
-  const username = 'peas';
+  const [username, setUsername] = React.useState('ariasilva');
+  const roteamento = useRouter();
 
   return (
     <>
-      <GlobalStyle />
+
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
-          backgroundColor: appConfig.theme.colors.primary[500],
-          backgroundImage: 'url(https://images.unsplash.com/photo-1515255384510-23e8b6a6ca3c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1332&q=80)',
+          backgroundImage: 'url(https://images.unsplash.com/photo-1587190612551-6ae2b5a59951?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)',
           backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
         }}
       >
@@ -85,31 +87,41 @@ export default function PaginaInicial() {
             width: '100%', maxWidth: '700px',
             borderRadius: '5px', padding: '32px', margin: '16px',
             boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-            backgroundColor: appConfig.theme.colors.neutrals[700],
+            backgroundColor: appConfig.theme.colors.neutrals['050'],
           }}
         >
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              console.log('Alguém submeteu o formulário.');
+              roteamento.push(`/chat?username=${username}`);
+            }}
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
             }}
           >
-            <Title tag="h2">Welcome!</Title>
-            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
+            <Titulo tag="h2">Boas vindas!</Titulo>
+            <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals['400'] }}>
               {appConfig.name}
             </Text>
 
+
             <TextField
+              value={username}
+              onChange={function (event) {
+                console.log('usuário digitou', event.target.value);
+                const valor = event.target.value;
+                setUsername(valor);
+              }}
               fullWidth
               textFieldColors={{
-                neutral: {
-                  textColor: appConfig.theme.colors.neutrals[200],
-                  mainColor: appConfig.theme.colors.neutrals[900],
-                  mainColorHighlight: appConfig.theme.colors.primary[500],
-                  backgroundColor: appConfig.theme.colors.neutrals[800],
-                },
+                textColor: appConfig.theme.colors.neutrals[200],
+                mainColor: appConfig.theme.colors.neutrals[200],
+                mainColorHighlight: appConfig.theme.colors.primary[300],
+                backgroundColor: appConfig.theme.colors.primary[400],
               }}
             />
             <Button
@@ -118,9 +130,9 @@ export default function PaginaInicial() {
               fullWidth
               buttonColors={{
                 contrastColor: appConfig.theme.colors.neutrals["000"],
-                mainColor: appConfig.theme.colors.primary[500],
-                mainColorLight: appConfig.theme.colors.primary[400],
-                mainColorStrong: appConfig.theme.colors.primary[600],
+                mainColor: appConfig.theme.colors.primary['300'],
+                mainColorLight: appConfig.theme.colors.primary[200],
+                mainColorStrong: appConfig.theme.colors.primary[200],
               }}
             />
           </Box>
@@ -135,9 +147,9 @@ export default function PaginaInicial() {
               alignItems: 'center',
               maxWidth: '200px',
               padding: '16px',
-              backgroundColor: appConfig.theme.colors.neutrals[800],
+              backgroundColor: appConfig.theme.colors.neutrals['100'],
               border: '1px solid',
-              borderColor: appConfig.theme.colors.neutrals[999],
+              borderColor: appConfig.theme.colors.neutrals['050'],
               borderRadius: '10px',
               flex: 1,
               minHeight: '240px',
@@ -153,8 +165,8 @@ export default function PaginaInicial() {
             <Text
               variant="body4"
               styleSheet={{
-                color: appConfig.theme.colors.neutrals[200],
-                backgroundColor: appConfig.theme.colors.neutrals[900],
+                color: appConfig.theme.colors.neutrals['050'],
+                backgroundColor: appConfig.theme.colors.primary[200],
                 padding: '3px 10px',
                 borderRadius: '1000px'
               }}
